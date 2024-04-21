13eade3a83332a2cad5a33068465de4c507c3efc
Ryan Fitzgerald <ryfitz@ymail.com>
2024-04-21 13:27:21 -0600

    token balance

--- test/Token.js
+++ test/Token.js
@@ -6,33 +6,39 @@ const tokens = (n)=>{
 }
 
 describe('Token', () => {
-  let token
+  let token, accounts, deployer
 
   beforeEach(async()=>{
     const Token = await ethers.getContractFactory('Token') 
     token = await Token.deploy('FITZ Coin', 'FTC', '1000000')
+
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
   })
 
   describe('Deployment', ()=>{
     const name = 'FITZ Coin'
     const symbol = 'FTC'
     const decimals = '18'
-    const totalSupply = ('1000000')
+    const totalSupply = tokens('1000000')
 
     it("has correct name", async () => {
-      expect(await token.name()).to.equal('FITZ Coin')
+      expect(await token.name()).to.equal(name)
     })
     
     it("has correct symbol", async () => {
-      expect(await token.symbol()).to.equal('FTC')
+      expect(await token.symbol()).to.equal(symbol)
     })
     
     it("has correct decimals", async () => {
-      expect(await token.decimals()).to.equal('18')
+      expect(await token.decimals()).to.equal(decimals)
     })
     
     it("has correct total supply", async () => {
-      expect(await token.totalSupply()).to.equal(tokens('1000000'))
+      expect(await token.totalSupply()).to.equal(totalSupply)
+    })
+    it("assigns total supply to deployer", async () => {
+      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
     })
   })
   })
