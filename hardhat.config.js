17a1e29da21a76e22def52383256bfd8fba9516a
Ryan Fitzgerald <ryfitz@ymail.com>
2024-04-23 01:13:59 -0600

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
 
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -8,6 +9,10 @@ require('dotenv').config();
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost:{}
+    localhost: {},
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(",")
+    }
   }
 };
